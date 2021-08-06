1607dae0bcd43f0bc17fce44a54e0e420c36247a
wbarbosa08 <wbarbosa08@gmail.com>
2021-08-05 23:15:35 -0300

    adicionando o projeto principal na raiz

---
+++ demo/index.js
@@ -0,0 +1,10 @@
+function onLoad() {
+  const dependencias = {
+    tela: Tela, // a classe tela é global
+    util: Util
+  }
+  //inicializamos o jogo da memória
+  const jogoDaMemoria = new JogoDaMemoria(dependencias)
+  jogoDaMemoria.inicializar()
+}
+window.onload = onLoad
